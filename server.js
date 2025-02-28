2006a0a09519417318f6105708808c7bc929daba
Shahid <shahidwazidh@gmail.com>
2025-02-28 21:39:18 +0530

    db connection

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ connectDB();
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/quiz", require("./routes/quizRoutes"));
 
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
