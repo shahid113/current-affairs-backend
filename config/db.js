74585a9ae09fb5a8e6d3ad1b79d4cdff5fce3839
Shahid <shahidwazidh@gmail.com>
2025-03-07 22:44:17 +0530

    dbname

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: 'currentaffairs'
   });
   return conn;
 };
